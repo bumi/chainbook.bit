9e6924876def07f4ede097041c5768813f0530ca
Michael Bumann <michael@railslove.com>
2015-08-26 20:25:01 +0200

    text

--- app.js
+++ app.js
@@ -111,8 +111,12 @@ var App = React.createClass({
             {friends}
           </ul>
           <div className="about">
-            <h3>About</h3>
-            <p>your namecoin and remotestorage based addressbook. <a href="https://github.com/bumi/about.bit#readme">Read more about the project on Github</a></p>
+            <h3>About:</h3>
+            <p>
+              about.bit is a <a href="http://namecoin.org">namecoin</a> and <a href="http://remotestorage.io">remotestorage</a> based addressbook. <a href="https://github.com/bumi/about.bit#readme">Read more about the project on Github</a>
+            </p>
+            <p><small>Wanted: <a href="http://github.com/bumi/about.bit">Help build a beautiful interface</a></small></p>
+            <p>Contact: <a href="http://michaelbumann.com">michaelbumann.com</a></p>
           </div>
         </div>
         <div id="profile-wrapper">
